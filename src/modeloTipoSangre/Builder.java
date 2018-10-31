5bab2bffd4f177c937cdd5843c8b9e1378804b5f
Florian Chávez <florian@chavezjuarez.com>
2018-10-30 18:09:32 -0600

    Clase del 31 de octubre

--- src/modeloTipoSangre/Builder.java
+++ src/modeloTipoSangre/Builder.java
@@ -6,6 +6,7 @@ import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.engine.environment.RunEnvironment;
 import repast.simphony.parameter.Parameters;
+import repast.simphony.random.RandomHelper;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.continuous.RandomCartesianAdder;
 
@@ -28,11 +29,37 @@ public class Builder implements ContextBuilder<Object>{
 				new repast.simphony.space.continuous.StrictBorders(),dimensionX,dimensionY);
 				
 		
-		
+		// Generar agentes
+			//Obtener las proporciones
+			double propA = params.getDouble("propA");
+			double propAB = params.getDouble("propAB");
+			double propB = params.getDouble("propB");
 		
 		for(int i=0;i<numeroAgentes;i++) {
-			context.add(new Individual("A","A"));
+			double r = RandomHelper.nextDoubleFromTo(0,1);
+			if(r<propA) {
+				context.add(new Individual("A"));
+			}
+			else if(r < propA+propB) {
+				context.add(new Individual("B"));
+			}
+			else if(r < propA + propB + propAB) {
+				context.add(new Individual("AB"));
+			}
+			else {
+				context.add(new Individual("O"));
+			}
+			
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		return context;
--- src/modeloTipoSangre/Individual.java
+++ src/modeloTipoSangre/Individual.java
@@ -1,5 +1,12 @@
 package modeloTipoSangre;
 
+import java.util.ArrayList;
+
+import repast.simphony.context.Context;
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.util.ContextUtils;
+
 public class Individual {
 	
 	String gene1;
@@ -13,6 +20,75 @@ public class Individual {
 		defineBloodType();
 	}
 
+	public Individual(String tipoSangre) {
+		this.tipoSangre = tipoSangre;
+		
+		// Generar una variable aleatoria para los casos no deterministicos
+		double r = RandomHelper.nextDoubleFromTo(0, 1);
+		if(this.tipoSangre=="A") {
+			this.gene1 = "A";
+			if(r<=1/3) {
+				this.gene2 = "A";
+			}
+			else {
+				this.gene2 = "O";
+			}
+		}// end if tipoSange==A
+		else if(this.tipoSangre=="B") { // usando una forma mas rapida de programar lo equivalente
+			this.gene1 = "B";
+			this.gene2 = (r<1/3) ? "B" : "O";
+		}
+		else if(this.tipoSangre=="AB") {
+			this.gene1 ="A";
+			this.gene2 ="B";
+		}
+		else if(this.tipoSangre=="O") {
+			this.gene1="O";
+			this.gene2="O";
+		}
+		else {
+			System.out.printf("Tipo de sangre desconocido: %s",this.tipoSangre);
+			System.exit(0); // solucion radical: para la ejecucion de java (incluyendo repast)
+		}
+	}
+
+	@ScheduledMethod(start=1,interval=1,shuffle=true,priority=50)
+	public void stepMatching() {
+		
+		// Obtener el 'context' de este agente
+		Context<Object> miContexto = ContextUtils.getContext(this);
+		ArrayList<Individual> candidatos = new ArrayList<Individual>();
+		
+		for(Object o: miContexto.getObjects(Individual.class)) {
+			if(o!=this) {
+				candidatos.add((Individual) o);
+			}
+		}
+		
+		//Ahora el array list contiene todos los individuos (menos el this)
+		Individual pareja = candidatos.get(0);
+		
+		// Ahora generamos los nuevos individuos
+		Individual child1 = new Individual(this.getOneGene(),pareja.getOneGene());
+		Individual child2 = new Individual(this.getOneGene(),pareja.getOneGene());
+		
+		
+		
+	}
+	
+	protected String getOneGene(){
+		double r = RandomHelper.nextDoubleFromTo(0, 1);
+		if(r<0.5) {
+			return this.gene1;
+		}
+		else {
+			return this.gene2;
+		}
+	}
+	
+	
+	
+	
 	private void defineBloodType() {
 		if((this.gene1=="A" && this.gene2=="A") ||
 				(this.gene1=="A" && this.gene2=="O") ||
@@ -31,7 +107,12 @@ public class Individual {
 			this.tipoSangre="AB";
 		}
 	}
+	
+	public String getTipoSangre() {
+		return this.tipoSangre;
+	}
 
+	
 	
 	
 	
